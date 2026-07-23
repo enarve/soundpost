3c3de26c199a24e52f260005e4fed72e845c5c66
Nikita <80875524+enarve@users.noreply.github.com>
2026-07-23 23:32:07 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,25 +1,30 @@
 from sqlmodel import SQLModel, Field
 
 class User(SQLModel, table=True):
-    __tablename__ = "users" # type: ignore
+    __tablename__ = "users"
+    
     id: int | None = Field(default=None, primary_key=True)
     username: str
     email: str
     password_hash: str
     created_at: str
+    
+    display_name: str | None
+    about_me: str | None
+    website: str | None
 
 # class Artist(SQLModel, table=True):
-#     __tablename__ = "artists" # type: ignore
+#     __tablename__ = "artists"
 #     pass
 
 # class Album(SQLModel, table=True):
-#     __tablename__ = "albums" # type: ignore
+#     __tablename__ = "albums"
 #     pass
 
 # class Track(SQLModel, table=True):
-#     __tablename__ = "tracks" # type: ignore
+#     __tablename__ = "tracks"
 #     pass
 
 # class Scrobble(SQLModel, table=True):
-#     __tablename__ = "scrobbles" # type: ignore
+#     __tablename__ = "scrobbles"
 #     pass
