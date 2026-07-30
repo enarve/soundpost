33337279a6e27f71bf4373f6e439b1067b16d42d
Nikita <80875524+enarve@users.noreply.github.com>
2026-07-30 22:10:20 +0200

    Database connect_args

--- app/db.py
+++ app/db.py
@@ -3,7 +3,8 @@ from sqlmodel import SQLModel, Session, create_engine
 from app.models import Artist
 
 database_url = "sqlite:///database.db"
-engine = create_engine(database_url, echo=True)
+connect_args = {"check_same_thread": False}
+engine = create_engine(database_url, echo=True, connect_args=connect_args)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
